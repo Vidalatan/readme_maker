5972683fffabe1f100465589bfa3b4e701422367
Vidalatan <vidalatan@gmail.com>
2022-03-24 15:05:36 -0500

    Initial Commit
    Start of readme maker application

---
+++ index.js
@@ -0,0 +1,15 @@
+const genMark = require('./utils/generateMarkdown');
+const fs = require('fs');
+const inquirer = require('inquirer')
+
+// TODO: Create an array of questions for user input
+const questions = [];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
